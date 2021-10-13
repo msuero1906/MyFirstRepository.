c4852821fcf2c5e3906dbcb6ab2037cdbe767598
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-10-13 18:26:27 -0400

    Added backgrounds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./styles.css";
 
 export default function App() {
+  const mystyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
   return (
-    <div className="App">
-      <h1>Hello my name is Melody!</h1>
-      <h2>Year: 2021 College: CCNY</h2>
+    <div style={mystyle} className="App">
+      <h1 style={{ color: "red" }} style={{ backgroundColor: "lightblue" }}>
+        Hello my name is Melody!
+      </h1>
+      <h2 style={{ color: "purple" }} style={{ backgroundColor: "green" }}>
+        {" "}
+        Year: 2021 College: CCNY
+      </h2>
     </div>
   );
 }
