0bff0a76c37bf0d564532cd74ecf84bed1bc7281
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-10-13 17:19:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello my name is Melody!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Year: 2021 College: CCNY</h2>
     </div>
   );
 }
