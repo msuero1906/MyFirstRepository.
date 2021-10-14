29d53270d5f6991e157acd2ffba76c25a7742fb6
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-10-14 18:08:38 -0400

    added image

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ export default function App() {
         Hello my name is Melody!
       </h1>
       <h2 style={{ color: "purple" }} style={{ backgroundColor: "green" }}>
-        {" "}
         Year: 2021 College: CCNY
       </h2>
+      <img src="https://static01.nyt.com/images/2016/08/30/nyregion/30CUNY3/30CUNY2-1472516337388-master768.jpg" />
     </div>
   );
 }
