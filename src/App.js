675529e34bc97243f005e1f2ddfc9a96cc94f4ac
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-10-14 21:10:23 -0400

    added anchor tag

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./styles.css";
 
 export default function App() {
   const mystyle = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
+    color: "black",
+    backgroundColor: "Burgundy",
     padding: "10px",
-    fontFamily: "Arial"
+    fontFamily: "Lucida Handwriting"
   };
   return (
     <div style={mystyle} className="App">
@@ -15,6 +15,12 @@ export default function App() {
       <h2 style={{ color: "purple" }} style={{ backgroundColor: "green" }}>
         Year: 2021 College: CCNY
       </h2>
+      <div onclick="pageLink()" class="box">
+        <a href="https://art.alphacoders.com/authors/view/1093">
+          Ayami Kojima Art
+        </a>
+      </div>
+
       <img src="https://static01.nyt.com/images/2016/08/30/nyregion/30CUNY3/30CUNY2-1472516337388-master768.jpg" />
     </div>
   );
